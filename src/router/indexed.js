5c8ace2e5855a33df3f163c89b17ad4b00179a9e
maphnew <zcm3118@gmail.com>
2020-02-27 17:14:04 +0900

    wavelist x and y

--- src/router/indexed.js
+++ src/router/indexed.js
@@ -12,6 +12,7 @@ const runPy = (startTime, stopTime, index_date, index_num) => {
         const pyprog = spawn('python', [getStatisticsPath, startTime, stopTime, index_date, index_num])
     
         pyprog.stdout.on('data', (data) => {
+            console.log('python data', data)
             resolve(data)
         })
         pyprog.stderr.on('data', (data) => {
@@ -20,36 +21,14 @@ const runPy = (startTime, stopTime, index_date, index_num) => {
     })
 }
 
-const getStatisticsQuery = async (indexDate, indexNum, parts) => {
-    const partsIndex = Object.keys(parts)
-    // console.log(partsIndex) // [0,1,2]
-    let query = `UPDATE WaveSplit SET features = JSON_SET(features,`
-    for (let k of partsIndex) {
-        startTime = parts[k]['startTime']
-        stopTime = parts[k]['stopTime']
-        // console.log(startTime, stopTime)
-        await runPy(startTime, stopTime, indexDate, indexNum).then((statistics) => {
-            const str = statistics.toString()
-            const replace = str.replace(/'/gi, "\"")
-            const json = JSON.parse(replace)
-            // console.log('json: ', json)
-            const key = Object.keys(json)
-            // console.log('key:', key)
-            let queryKey = ''
-            for(let s of key) {
-                const stat = json[s]
-                queryKey += `'$.${k}.${s}', ${stat},`
-            }
-            // console.log(queryKey)
-            query += queryKey
-        }).catch((e) => {
-            console.log(e)
-        })
-    }
-    query = query.slice(0, -1)
-    query += `) WHERE index_date = '${indexDate}' AND index_num = ${indexNum};`
-    // console.log(query)
-    return query
+const getStatistics = async (indexDate, indexNum, parts) => {
+    await runPy(indexDate, indexNum, parts).then( async (statistics) => {
+        console.log(statistics)
+    }).catch((e) => {
+        console.log(e)
+    })
+
+
 }
 
 router.get('/indexed', (req, res) => {
@@ -66,7 +45,7 @@ router.post('/indexed/waveform', async (req,res) => {
     const start = moment(startTime).format('YYYY-MM-DD HH:mm:ss.SSS')
     const stop = moment(stopTime).format('YYYY-MM-DD HH:mm:ss.SSS')
     const queryWaveForm = `
-        SELECT DataSavedTime, ${column}
+        SELECT DataSavedTime as x, ${column} as y
         FROM ${table}
         WHERE DataSavedTime BETWEEN '${start}' AND '${stop}'
     `
@@ -78,24 +57,25 @@ router.post('/indexed/waveform', async (req,res) => {
     })
 })
 
-router.patch('/indexed/test', async (req, res) => {
-    // console.log(req.body)
-    const indexDate = moment(req.body.index_date).format('YYYY-MM-DD')
-    const indexNum = req.body.index_num
-    const parts = req.body.parts
+// router.patch('/indexed/test', async (req, res) => {
+//     // console.log(req.body)
+//     const indexDate = moment(req.body.index_date).format('YYYY-MM-DD')
+//     const indexNum = req.body.index_num
+//     const parts = req.body.parts
     
-    // console.log(parts)
-    const query = await getStatisticsQuery(indexDate,indexNum,parts)
-    // console.log("query",query)
-    await dbUpdate(query).then((result) => {
-        console.log(result)
-        res.status(200).send('ok')
-    }).catch((e) => {
-        res.status(400).send(e)
-    })
-})
+//     // console.log(parts)
+//     const query = await getStatisticsQuery(indexDate,indexNum,parts)
+//     console.log("getStatisticsQuery",query)
+//     await dbUpdate(query).then((result) => {
+//         console.log('result: ', result)
+//         res.status(200).send('ok')
+//     }).catch((e) => {
+//         res.status(400).send(e)
+//     })
+// })
+
 router.patch('/indexed/splitlist', async (req, res) => {
-    console.log(req.body)
+    console.log('req.body:', req.body)
     const indexDate = moment(req.body.index_date).format('YYYY-MM-DD')
     const indexNum = req.body.index_num
     const parts = req.body.parts
@@ -116,16 +96,11 @@ router.patch('/indexed/splitlist', async (req, res) => {
         WHERE index_date = '${indexDate}' AND index_num = ${indexNum};
     `
     console.log(queryUpdateWaveList)
-    const queryStatistics = await getStatisticsQuery(indexDate,indexNum,parts)
     await dbUpdate(queryUpdateWaveList).then((result) => {
         return result
     }).then( async (result) => {
-        if (result) {
-            await dbUpdate(queryStatistics).then((result) => {
-                console.log(result)
-            }).catch((e) => {
-                console.log(e)
-            })
+        if(result) {
+            await getStatistics(indexDate,indexNum,parts)
         }
         res.status(200).send('ok')
     }).catch((e) => {
