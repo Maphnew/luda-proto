ebd4971874e917090892556e009dfeae966ddeb7
heejin <gmmlwls6012@naver.com>
2020-03-12 18:03:58 +0900

    fetch modify and graphTable save error

--- client/src/components/Indexed/WaveListTable.js
+++ client/src/components/Indexed/WaveListTable.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 import equal from 'fast-deep-equal';
-import { Link, Route, BrowserRouter as Router } from 'react-router-dom';
-import Indexed from './Indexed';
-import GraphTable from './GraphTable';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 class WaveListTable extends React.Component {
   state = {
     selected:{},
+    json: [],
     data: [],
-    graphData: []
+    waveinfo:''
   };
 
   onRowSelect = async(row, isSelected) => {
@@ -18,7 +17,26 @@ class WaveListTable extends React.Component {
       var requiredPattern = 'YYYY-MM-DD HH:mm:ss.SSS';
       row.index_date = moment(row.index_date).format(requiredPattern);
       await this.setState({selected: row});     
-      this.props.onGraphData(this.state.selected)
+      const params = { "tagName": this.state.Item, "index_date": row.index_date, "index_num": row.index_num }
+      //  console.log('params',params);
+      fetch("http://192.168.100.175:5000/indexed/waveinfo", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': '*/*'
+        },
+        body:JSON.stringify(params)      
+      })
+        .then(response => response.json())
+        .then((json) => {
+          JSON.stringify(json.map(function (record, idx) {
+            record.idx = idx
+            return record;
+          })); 
+          this.setState({ waveinfo: json })
+          this.props.onGraphData(this.state.selected, this.state.waveinfo)
+        })
+        .catch(err => console.log(err));
     } else {
       this.props.onGraphData({})
     }
@@ -27,8 +45,9 @@ class WaveListTable extends React.Component {
   componentWillReceiveProps = async (nextProps) => {
     if (!equal(this.props.wavelist, nextProps.wavelist)) // Check if it's a new user, you can also use some unique property, like the ID  (this.props.user.id !== prevProps.user.id)
     {
+      await this.setState({ Item: nextProps.ItemData })
       await this.setState({ data: nextProps.wavelist })      
-      //console.log("nextProps.wavelist",nextProps.wavelist)
+      // console.log("nextProps.wavelist",this.state.data, "Item", this.state.Item)
       if (this.state.selected.idx !== undefined) {
         this.props.onGraphData(nextProps.wavelist[this.state.selected.idx])
       }
@@ -57,9 +76,6 @@ class WaveListTable extends React.Component {
           </TableHeaderColumn>
             <TableHeaderColumn dataField='stopTime'>
               StopTime
-          </TableHeaderColumn>
-            <TableHeaderColumn dataField='split'>
-              split
           </TableHeaderColumn>
           </BootstrapTable>
         </div>
